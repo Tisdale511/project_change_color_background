4e7c05cb936ad7b4abe8928b8da3d2d2c505eff1
Tisdale511 <tisdalefry@gmail.com>
2021-01-19 11:14:26 -0500

    challenge completed

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
-//Choose a random color
-const button = document.querySelector('button')
-const body = document.querySelector('body')
-const colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple']
+const button = document.querySelector('button');
+const body = document.querySelector('body');
+const colors = ['red', 'blue', 'yellow', 'orange', 'green', 'purple']
 
 body.style.backgroundColor = 'violet'
-button.addEventListener('click', changeBackground)
 
 function changeBackground(){
-const colorIndex= parseInt(Math.random()*colors.length)
-body.style.backgroundColor = colors[colorIndex]
+    const colorIndex = parseInt(Math.random()*colors.length, 10)
+    body.style.backgroundColor = colors[colorIndex]
 }
+
+button.addEventListener('click', changeBackground)
+
